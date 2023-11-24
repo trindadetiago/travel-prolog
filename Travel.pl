4be018cc2ed9f56a3ac3a2bc6ed696891a85513a
Tiago Trindade <111463808+trindadetiago@users.noreply.github.com>
2023-11-24 10:30:06 -0300

    Add files via upload

---
+++ Travel.pl
@@ -0,0 +1,379 @@
+cidade(paris,
+        atividades(turismo, museus, compras, arte),
+        caracteristicas(mediocusto, segurancaalta, popularidadealta),
+        detalhes(frances, franca, europa),
+        clima(frio, ameno, quente)).
+
+cidade(roma,
+        atividades(museus, historia, arte, culinaria),
+        caracteristicas(mediocusto, segurancaalta, popularidademedia),
+        detalhes(italiano, italia, europa),
+        clima(ameno, quente, quente)).
+
+cidade(tokyo,
+        atividades(compras, cultura, tecnologia, culinaria),
+        caracteristicas(altocusto, segurancaalta, popularidadealta),
+        detalhes(japones, japao, asia),
+        clima(frio, ameno, quente)).
+
+cidade(nova_york,
+        atividades(museus, teatros, noturna, cultura),
+        caracteristicas(altocusto, segurancamedia, popularidadealta),
+        detalhes(ingles, eua, america_do_norte),
+        clima(frio, ameno, quente)).
+
+cidade(amsterda,
+        atividades(turismo, ciclismo, cafeterias, cultura),
+        caracteristicas(mediocusto, segurancaalta, popularidademedia),
+        detalhes(holandes, paises_baixos, europa),
+        clima(frio, ameno, ameno)).
+
+cidade(rio_de_janeiro,
+        atividades(praias, samba, passeios, cultura),
+        caracteristicas(mediocusto, segurancabaixa, popularidademedia),
+        detalhes(portugues, brasil, america_do_sul),
+        clima(quente, quente, quente)).
+
+cidade(cidade_do_cabo,
+        atividades(safaris, culinaria, passeios, natureza),
+        caracteristicas(mediocusto, segurancabaixa, popularidademedia),
+        detalhes(ingles, africa_do_sul, africa),
+        clima(ameno, ameno, quente)).
+
+cidade(sydney,
+        atividades(natureza, surf, praias, turismo),
+        caracteristicas(altocusto, segurancaalta, popularidadealta),
+        detalhes(ingles, australia, oceania),
+        clima(quente, ameno, quente)).
+
+cidade(dubai,
+        atividades(compras, tecnologia, deserto, cultura),
+        caracteristicas(altocusto, segurancaalta, popularidadealta),
+        detalhes(arabe, emirados_arabes_unidos, asia),
+        clima(quente, quente, quente)).
+
+cidade(cairo,
+        atividades(historia, museus, passeios, deserto),
+        caracteristicas(baixocusto, segurancamedia, popularidademedia),
+        detalhes(arabe, egito, africa),
+        clima(ameno, quente, quente)).
+
+cidade(berlim,
+        atividades(historia, noturna, arte, turismo),
+        caracteristicas(mediocusto, segurancaalta, popularidadealta),
+        detalhes(alemao, alemanha, europa),
+        clima(frio, ameno, ameno)).
+
+cidade(bangkok,
+        atividades(templos, compras, culinaria, cultura),
+        caracteristicas(baixocusto, segurancamedia, popularidadebaixa),
+        detalhes(tailandes, tailandia, asia),
+        clima(quente, chuvoso, chuvoso)).
+
+cidade(lisboa,
+        atividades(historia, culinaria, cultura, turismo),
+        caracteristicas(mediocusto, segurancaalta, popularidademedia),
+        detalhes(portugues, portugal, europa),
+        clima(ameno, quente, ameno)).
+
+cidade(buenos_aires,
+        atividades(tango, arquitetura, passeios, arte),
+        caracteristicas(mediocusto, segurancabaixa, popularidadealta),
+        detalhes(espanhol, argentina, america_do_sul),
+        clima(quente, ameno, frio)).
+
+cidade(londres,
+        atividades(museus, teatros, compras, turismo),
+        caracteristicas(altocusto, segurancamedia, popularidadealta),
+        detalhes(ingles, reino_unido, europa),
+        clima(frio, ameno, ameno)).
+
+cidade(sao_paulo,
+        atividades(culinaria, noturna, turismo, arte),
+        caracteristicas(altocusto, segurancabaixa, popularidadealta),
+        detalhes(portugues, brasil, america_do_sul),
+        clima(ameno, quente, quente)).
+
+cidade(moscou,
+        atividades(arquitetura, historia, noturna, turismo),
+        caracteristicas(mediocusto, segurancaalta, popularidadealta),
+        detalhes(russo, russia, europa),
+        clima(frio, frio, ameno)).
+
+cidade(istambul,
+        atividades(cultura, compras, natureza, passeios),
+        caracteristicas(mediocusto, segurancaalta, popularidadealta),
+        detalhes(turco, turquia, europa),
+        clima(frio, ameno, ameno)).
+
+cidade(vancouver,
+        atividades(arte, natureza, culinaria, arte),
+        caracteristicas(baixocusto, segurancamedia, popularidademedia),
+        detalhes(ingles, canada, america_do_norte),
+        clima(frio, ameno, ameno)).
+
+cidade(marrakech,
+        atividades(compras, arquitetura, cultura, historia),
+        caracteristicas(baixocusto, segurancabaixa, popularidadebaixa),
+        detalhes(arabe, marrocos, africa),
+        clima(quente, quente, quente)).
+
+cidade(auckland,
+        atividades(natureza, passeios, culinaria, cultura),
+        caracteristicas(baixocusto, segurancamedia, popularidadebaixa),
+        detalhes(ingles, nova_zelandia, oceania),
+        clima(quente, ameno, ameno)).
+
+cidade(singapura,
+        atividades(arquitetura, culinaria, natureza, tecnologia),
+        caracteristicas(altocusto, segurancaalta, popularidadealta),
+        detalhes(ingles, singapura, asia),
+        clima(quente, chuvoso, chuvoso)).
+
+cidade(praga,
+        atividades(historia, culinaria, cultura, turismo),
+        caracteristicas(mediocusto, segurancaalta, popularidademedia),
+        detalhes(tcheco, republica_tcheca, europa),
+        clima(frio, ameno, ameno)).
+
+cidade(toronto,
+        atividades(arquitetura, cultura, arte, turismo),
+        caracteristicas(altocusto, segurancamedia, popularidadealta),
+        detalhes(ingles, canada, america_do_norte),
+        clima(frio, quente, ameno)).
+
+cidade(miami,
+        atividades(praias, noturna, compras, passeios),
+        caracteristicas(altocusto, segurancamedia, popularidadealta),
+        detalhes(ingles, eua, america_do_norte),
+        clima(quente, ameno, ameno)).
+
+cidade(hong_kong,
+        atividades(tecnologia, culinaria, compras, passeios),
+        caracteristicas(altocusto, segurancamedia, popularidadealta),
+        detalhes(cantonese, china, asia),
+        clima(ameno, chuvoso, chuvoso)).
+
+cidade(atenas,
+        atividades(historia, culinaria, praias, cultura),
+        caracteristicas(mediocusto, segurancaalta, popularidademedia),
+        detalhes(grego, grecia, europa),
+        clima(ameno, quente, quente)).
+
+cidade(bali,
+        atividades(praias, cultura, culinaria, museus),
+        caracteristicas(mediocusto, segurancamedia, popularidademedia),
+        detalhes(balines, indonesia, asia),
+        clima(quente, chuvoso, chuvoso)).
+
+cidade(viena,
+        atividades(museus, arquitetura, cultura, arte),
+        caracteristicas(altocusto, segurancaalta, popularidadealta),
+        detalhes(alemao, austria, europa),
+        clima(frio, ameno, ameno)).
+
+cidade(santiago,
+        atividades(culinaria, natureza, cultura, arte),
+        caracteristicas(mediocusto, segurancamedia, popularidadealta),
+        detalhes(espanhol, chile, america_do_sul),
+        clima(frio, ameno, ameno)).
+
+cidade(joao_pessoa,
+        atividades(praias, culinaria, cultura, passeios),
+        caracteristicas(baixocusto, segurancabaixa, popularidadebaixa),
+        detalhes(portugues, brasil, america_do_sul),
+        clima(quente, quente, quente)).
+
+cidade(madrid,
+        atividades(historia, museus, compras, noturna),
+        caracteristicas(mediocusto, segurancaalta, popularidadealta),
+        detalhes(espanhol, espanha, europa),
+        clima(ameno, quente, ameno)).
+
+cidade(salvador,
+        atividades(praias, cultura, arte, culinaria),
+        caracteristicas(mediocusto, segurancabaixa, popularidademedia),
+        detalhes(portugues, brasil, america_do_sul),
+        clima(quente, quente, quente)).
+
+cidade(montevideu,
+        atividades(praias, arquitetura, cultura, culinaria),
+        caracteristicas(mediocusto, segurancaalta, popularidademedia),
+        detalhes(espanhol, uruguai, america_do_sul),
+        clima(ameno, ameno, ameno)).
+
+cidade(quebec,
+        atividades(arquitetura, cultura, culinaria, esportes),
+        caracteristicas(baixocusto, segurancaalta, popularidademedia),
+        detalhes(frances, canada, america_do_norte),
+        clima(frio, ameno, ameno)).
+
+cidade(havana,
+        atividades(cultura, arquitetura, praias, arte),
+        caracteristicas(baixocusto, segurancamedia, popularidademedia),
+        detalhes(espanhol, cuba, america_do_norte),
+        clima(ameno, quente, ameno)).
+
+cidade(beirute,
+        atividades(cultura, culinaria, historia, noturna),
+        caracteristicas(baixocusto, segurancaalta, popularidadebaixa),
+        detalhes(arabe, libano, asia),
+        clima(ameno, quente, ameno)).
+
+cidade(munique,
+        atividades(culinaria, arquitetura, cultura, natureza),
+        caracteristicas(altocusto, segurancaalta, popularidadealta),
+        detalhes(alemao, alemanha, europa),
+        clima(frio, ameno, ameno)).
+
+cidade(manaus,
+        atividades(natureza, cultura, passeios, culinaria),
+        caracteristicas(mediocusto, segurancaalta, popularidademedia),
+        detalhes(portugues, brasil, america_do_sul),
+        clima(quente, chuvoso, chuvoso)).
+
+
+cidade_custo(Custo, Cidade) :-
+    cidade(Cidade,
+        _,
+        caracteristicas(Custo, _, _),
+        _,
+        _).
+
+cidade_popularidade(Popularidade, Cidade) :-
+    cidade(Cidade,
+        _,
+        caracteristicas(_, _, Popularidade),
+        _,
+        _).
+
+cidade_seguranca(Seguranca, Cidade) :-
+    cidade(Cidade,
+        _,
+        caracteristicas(_,Seguranca, _),
+        _,
+        _).
+
+
+
+cidade_clima_inicio(Clima, Cidade) :-
+    cidade(Cidade,
+        _,
+        _,
+        _,
+        clima(Clima, _, _)).
+
+cidade_clima_meio(Clima, Cidade) :-
+    cidade(Cidade,
+        _,
+        _,
+        _,
+        clima(_, Clima, _)).
+
+cidade_clima_fim(Clima, Cidade) :-
+    cidade(Cidade,
+        _,
+        _,
+        _,
+        clima(_, _, Clima)).
+
+
+
+
+cidade_lingua(Lingua, Cidade) :-
+    cidade(Cidade,
+        _,
+        _,
+        detalhes(Lingua,_,_),
+        _).
+
+cidade_pais(Pais, Cidade) :-
+    cidade(Cidade,
+        _,
+        _,
+        detalhes(_,Pais,_),
+        _).
+
+cidade_continente(Continente, Cidade) :-
+    cidade(Cidade,
+        _,
+        _,
+        detalhes(_,_,Continente),
+        _).
+
+
+
+cidade_nome(Cidade) :-
+    cidade(Cidade,
+        _,
+        _,
+        _,
+        _).
+
+
+atividades_cidade(Cidade, Atividades) :-
+    cidade(Cidade,
+        Atividades,
+        _,_,_),
+    write(Atividades).
+
+detalhes_cidade(Cidade, Detalhes) :-
+    cidade(Cidade,_,_,Detalhes,_),
+    write(Detalhes).
+
+main :-
+    write('Digite as caracteristicas da cidade que deseja visitar: '), nl,
+    write('Custo (baixo (padrao), medio, alto): '),
+    read(Custo),
+    write('Segurança (baixa, media, alta (padrao)): '),
+    read(Seguranca),
+    write('Popularidade (baixa, media, alta (padrao)): '),
+    read(Popularidade),
+    (
+        Popularidade == baixa ->
+        NovaPopularidade = popularidadebaixa
+        ;
+        Popularidade == media ->
+        NovaPopularidade = popularidademedia
+        ;
+        NovaPopularidade = popularidadealta
+    ),
+    (
+        Seguranca == baixa ->
+        NovaSeguranca = segurancabaixa
+        ;
+        Seguranca == media ->
+        NovaSeguranca = segurancamedia
+        ;
+        NovaSeguranca = segurancaalta
+    ),
+    (
+        Custo == alto ->
+        NovoCusto = altocusto
+        ;
+        Custo == medio ->
+        NovoCusto = mediocusto
+        ;
+        NovoCusto = baixocusto
+
+    ),
+    write('Epoca do ano (inicio, meio, fim ou qualquer): '),
+    read(Epoca),
+    (    
+        Epoca == inicio ->
+        write('Clima no início do ano: (frio, ameno, quente, chuvoso ou qualquer)'),
+        read(ClimaInicio)
+        ;
+        Epoca == meio ->
+        write('Clima no meio do ano: (frio, ameno, quente, chuvoso ou qualquer)'),
+        read(ClimaMeio)
+        ;
+        Epoca == fim ->
+        write('Clima no fim do ano: (frio, ameno, quente, chuvoso ou qualquer)'),
+        read(ClimaFim)
+    ),
+    nl, write('Cidades com as características desejadas:'), nl,
+    cidade(Nome, _, caracteristicas(NovoCusto, NovaSeguranca, NovaPopularidade), _, clima(_, _, _)),
+    write('Cidade: '), write(Nome), write('             '), atividades_cidade(Nome, Atividades), write('            '), detalhes_cidade(Nome, Detalhes), nl,
+    fail.
+#cidade(Nome, _, caracteristicas(altocusto, segurancaalta, popularidadealta), _, clima(quente, _, _))
